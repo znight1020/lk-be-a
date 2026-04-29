9acf995fec037b26f74b6f57cd24ebb49ba2b61f
이현수 <znight1020@naver.com>
2026-04-29 16:26:21 +0900

    refactor: 수강 예외 메시지 개선

--- src/main/java/leehs/course/core/enrollment/domain/exception/error/EnrollmentError.java
+++ src/main/java/leehs/course/core/enrollment/domain/exception/error/EnrollmentError.java
@@ -8,10 +8,10 @@ public enum EnrollmentError {
     ENROLLMENT_STATUS_NOT_PENDING(400, "신청 대기 상태가 아닙니다"),
     ENROLLMENT_STATUS_ALREADY_CANCELLED(400, "이미 취소된 상태입니다"),
 
-    ENROLLMENT_FORBIDDEN(403, "수강 신청 권한이 없습니다"),
-    ENROLLMENT_NOT_OWNER(403, "수강 신청 소유자가 아닙니다"),
+    ENROLLMENT_FORBIDDEN(403, "수강 신청은 학생만 가능합니다"),
+    ENROLLMENT_NOT_OWNER(403, "타인의 수강 신청 내역에 접근할 수 없습니다"),
 
-    ENROLLMENT_NOT_FOUND(404, "수강 신청을 찾을 수 없습니다"),
+    ENROLLMENT_NOT_FOUND(404, "수강 신청 내역을 찾을 수 없습니다"),
 
     ENROLLMENT_ALREADY_EXISTS(409, "이미 수강 신청한 강의입니다"),
     ENROLLMENT_CAPACITY_EXCEEDED(409, "정원이 가득 차 수강 신청할 수 없습니다"),
